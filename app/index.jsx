8fedafae81a6c27d1e1c7f81d4679af01f553c95
ethan-casselman <escasse@clemson.edu>
2025-10-19 16:29:59 -0400

    added about page

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,36 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
+import Narwhal from '../assets/img/narwhal.png'
+import {Link} from 'expo-router'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+        <Image source={Narwhal} style={styles.img}></Image>
+      <Text style={[styles.title, {color: 'green'}]}>The Number 1</Text>
+      <Text style={{marginTop: 10, marginBottom: 30}}>
+        Workout App
+        </Text>
+
+        <Link href="/about">About Page</Link>
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 18
+    },
+    img: {
+        marginVertical: 20,
+    },
+    
+})
