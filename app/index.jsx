961ed3bf5bd8e5f834230924f0840719a7fbed6d
ethan-casselman <escasse@clemson.edu>
2025-10-25 16:57:42 -0400

    added first parts of main page

--- app/index.jsx
+++ app/index.jsx
@@ -1,36 +1,81 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { StyleSheet, ScrollView, View, } from 'react-native'
 import React from 'react'
-import Narwhal from '../assets/img/narwhal.png'
-import {Link} from 'expo-router'
+import {Provider as PaperProvider, Button, Appbar, Card, Text, Avatar} from 'react-native-paper';
 
 const Home = () => {
+  const handleMenu = () => alert('Menu Pressed');
+  const handlesSignIn = () => alert('Sign In Pressed');
   return (
-    <View style={styles.container}>
-        <Image source={Narwhal} style={styles.img}></Image>
-      <Text style={[styles.title, {color: 'green'}]}>The Number 1</Text>
-      <Text style={{marginTop: 10, marginBottom: 30}}>
-        Workout App
-        </Text>
+    <PaperProvider>
+      <View style={styles.container}>
+        <Appbar.Header style={styles.header}>
+          <Appbar.Action icon="menu" onPress={handleMenu}/>
+          <Appbar.Content title="XPGains" titleStyle={styles.title}/>
+          <Appbar.Action icon="account-circle" onPress={handlesSignIn} />
+        </Appbar.Header>
 
-        <Link href="/about">About Page</Link>
+        <ScrollView contentContainerStyle={styles.body}>
+          <Card style ={styles.signInCard}>
+            <Card.Title 
+            title="Welcome to XPGains!"
+            left={(props) => <Avatar.Icon {...props} icon="account"/>}
+            titleStyle={{fontWeight: '700'}}
+            />
+            <Card.Content>
+              <Text style={styles.signInSubtitle}>
+              Track your progress and level up your fitness journey by signing in or creating
+              your account.
+              </Text>
+            </Card.Content>
+            <Card.Actions style={styles.center}>
+              <Button mode="contained" onPress={handlesSignIn}
+              >
+                Sign In / Create Account
+              </Button>
+            </Card.Actions>
+          </Card>
+        
+        </ScrollView>
     </View>
-  )
-}
+    </PaperProvider>
+  );
+};
 
 export default Home
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
+    },
+    header: {
+      backgroundColor: '#1e1e1e',
     },
     title: {
-        fontWeight: 'bold',
-        fontSize: 18
+      textAlign: 'center',
+      fontWeight: 'bold',
+      fontSize: 20,
+      color: '#ffffff',
     },
-    img: {
-        marginVertical: 20,
+    body: {
+      padding: 16,
+      alignItems: 'center',
     },
+    signInCard: {
+      width: '95%',
+      marginVertical: 12,
+    },
+    featureCard: {
+      width: '95%',
+      marginVertical: 10,
+    },
+    center: {
+      justifyContent: 'center',
+    },
+    signInSubtitle: {
+    marginTop: 4,
+    marginBottom: 8,
+    flexWrap: 'wrap',
+    lineHeight: 20,
+},
     
-})
+});
